66fab9e22e82ae280741046d381ce67cc45ea017
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-03-23 09:22:27 +0100

    Create after-instal.sh

---
+++ after-instal.sh
@@ -0,0 +1,4 @@
+sudo wget --output-document=/usr/share/backgrounds/fedora-workstation/now2y.jpg https://i.pinimg.com/originals/3b/8a/d2/3b8ad2c7b1be2caf24321c852103598a.jpg
+sudo wget --output-document=/home/$USER/.config/gconf/dconf.ini https://raw.githubusercontent.com/ShogoXY/kickstart_fedora/main/dconf.ini
+
+dconf load / < /home/$USER/.config/gconf/dconf.ini
