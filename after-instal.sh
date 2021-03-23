9ad24b8220acc1c28a79226b77cd008b84a8c2e1
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-03-23 14:22:55 +0100

    Update after-instal.sh

--- after-instal.sh
+++ after-instal.sh
@@ -9,7 +9,6 @@ dconf load / < /home/$USER/.config/gconf/dconf.ini
 
 sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
 sudo echo "fastestmirror=1" >> /etc/dnf/dnf.conf
-sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo systemctl disable NetworkManager-wait-online.service
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
