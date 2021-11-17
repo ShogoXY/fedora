e4baed3916a60e02df378b09deae94cb22892d1f
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-11-17 13:08:18 +0100

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -10,7 +10,7 @@ elif [[ $SYS =~ "Fedora" ]]; then
 VAR=dnf
 sudo VAR install git vim -y
 sudo VAR install cmake gcc-c++ make python3-devel -y 
-
+fi
 
 sudo wget --output-document=/home/$USER/.vimrc https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc_2 -y
 
