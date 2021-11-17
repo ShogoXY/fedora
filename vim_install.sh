f654e744058c8af8832dd4e038da01be9bde6bb7
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-11-17 14:57:53 +0100

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -4,13 +4,13 @@ SYS=` cat /etc/*-release 2> /dev/null | grep PRETTY_NAME `
 
 if [[ $SYS =~ "Debian" ]]; then
 VAR=apt
-sudo VAR install git vim -y
-sudo VAR install build-essential cmake vim-nox python3-dev -y
+sudo $VAR install git vim -y
+sudo $VAR install build-essential cmake vim-nox python3-dev -y
 
 elif [[ $SYS =~ "Fedora" ]]; then
 VAR=dnf
-sudo VAR install git vim -y
-sudo VAR install cmake gcc-c++ make python3-devel -y 
+sudo $VAR install git vim -y
+sudo $VAR install cmake gcc-c++ make python3-devel -y 
 fi
 
 sudo wget --output-document=/home/$USER/.vimrc https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc_2
