cc1e095ef304c377c5200055d0c864d19b98773c
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-11-17 15:41:09 +0100

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -14,10 +14,13 @@ sudo $VAR install git vim -y
 sudo $VAR install cmake gcc-c++ make python3-devel -y 
 fi
 
-sudo wget --output-document=/home/$USER/.vimrc https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc_2
+sudo wget --output-document=/home/$USER/.vimrc https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc_3
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cd ~/.vim/bundle/YouCompleteMe/;python3 install.py --all
 vim +PluginInstall +qall
+
+sudo wget --output-document=/home/$USER/.vimrc https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc_2
+
 vim .vimrc
