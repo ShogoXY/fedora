c746b9bd783106e9781a218e316326fa85b16afd
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-11-17 12:44:58 +0100

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc
 
-sudo bash -c `\
-apt install vim vim-gui-common -y
-apt install build-essential cmake vim-nox python3-dev -y
-\`
+if [[ $SYS =~ "Debian" ]]; then
+VAR=apt
+sudo VAR install git vim -y
+sudo VAR install build-essential cmake vim-nox python3-dev -y
+
+elif [[ $SYS =~ "Fedora" ]]; then
+VAR=dnf
+sudo VAR install git vim -y
+sudo VAR install cmake gcc-c++ make python3-devel -y 
+
+
+sudo wget --output-document=/home/$USER/.vimrc https://raw.githubusercontent.com/ShogoXY/fedora/main/.vimrc_2 -y
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim -y
+
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
+vim +PluginInstall +qall
