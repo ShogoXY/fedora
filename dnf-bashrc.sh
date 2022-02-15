9040fa9d451740d6c2fbac9f4f6625c560d7d48a
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-02-15 13:25:04 +0100

    Update dnf-bashrc.sh

--- dnf-bashrc.sh
+++ dnf-bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/ShogoXY/fedora/main/.dnfinstall
+wget https://raw.githubusercontent.com/ShogoXY/fedora/main/.dnfinstall.sh
 
 echo \alias dnfinstall=sh ~/.dnfinstall >> ~/.bashrc
 
