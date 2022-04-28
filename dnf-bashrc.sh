9b5e185747c6365fa4cef774a2aff6028d08bef1
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-04-28 09:23:26 +0200

    update

--- dnf-bashrc.sh
+++ dnf-bashrc.sh
@@ -3,7 +3,7 @@
 wget https://raw.githubusercontent.com/ShogoXY/fedora/main/.dnfinstall
 sudo dnf install fzf -y
 
-echo 'alias dnfinstall=\"bash ~/.dnfinstall\"' >> ~/.bashrc
+echo 'alias dnfinstall="bash ~/.dnfinstall"' >> ~/.bashrc
 
 
 echo "alias canon='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0' " >> ~/.bashrc
