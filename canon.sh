af938a907f668fe717ca4212c901b4d8cbcb29fb
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-07-25 21:44:05 +0200

    add chose fedora debian

--- canon.sh
+++ canon.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install gphoto2 v4l2loopback-utils v4l2loopback-dkms ffmpeg
+SYS=` cat /etc/*-release 2> /dev/null | grep PRETTY_NAME `
+de=$(echo $DESKTOP_SESSION)
+
+
+if [[ $SYS =~ "Debian" ]]; then
+
+    sudo apt-get update -y
+    sudo apt-get install gphoto2 v4l2loopback-utils v4l2loopback-dkms ffmpeg
+
+elif [[ $SYS =~ "Fedora" ]]; then
+    sudo dnf update -y
+    sudo dnf install gphoto2 v4l2loopback ffmpeg
+ fi
+     
+
+#sudo apt-get install gphoto2 v4l2loopback-utils v4l2loopback-dkms ffmpeg
 #sudo dnf install gphoto2 v4l2loopback ffmpeg
 
 sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
