0412905cecad8243bc1d7b5bb79fc4ad716681d5
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-07-25 21:41:42 +0200

    add touch

--- canon.sh
+++ canon.sh
@@ -5,6 +5,7 @@ sudo apt-get install gphoto2 v4l2loopback-utils v4l2loopback-dkms ffmpeg
 
 sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
 sudo echo "dslr-webcam" >> /etc/modules
+sudo touch /etc/modprobe.d/dslr-webcam.conf
 sudo printf "# Module options for Video4Linux, needed for our DSLR Webcam
 alias dslr-webcam v4l2loopback
 options v4l2loopback exclusive_caps=1 max_buffers=2" >> /etc/modprobe.d/dslr-webcam.conf
