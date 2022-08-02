c3b6d9847fb88bcb554f9aedec65623d23702f22
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-08-02 10:06:01 +0200

    Update dnf-bashrc.sh

--- dnf-bashrc.sh
+++ dnf-bashrc.sh
@@ -6,7 +6,7 @@ sudo dnf install fzf -y
 echo 'alias dnfinstall="bash ~/.dnfinstall"' >> ~/.bashrc
 
 
-echo "alias canon='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0' " >> ~/.bashrc
+#echo "alias canon='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0' " >> ~/.bashrc
 
 
 source ~/.bashrc
