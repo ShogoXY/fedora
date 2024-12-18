1bda8ef371e01c48fb3dc9783311555d63bd800f
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2024-12-18 22:08:01 +0100

    Update and rename fedora_after_install.sh to fedora_laptop_install.sh

--- fedora_laptop_install.sh
+++ fedora_laptop_install.sh
@@ -5,8 +5,22 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo 
 
 sudo dnf update -y
+packages=$(printf "
+thunderbird
+onedrive
+git
+mpv
+mscore-fonts-all
+curl
+cabextract
+xorg-x11-font-utils
+fontconfig
+flameshot
+neovim
+")
 
-sudo dnf install thunderbird onedrive git mpv mscore-fonts-all curl cabextract xorg-x11-font-utils fontconfig flameshot neovim -y
+
+sudo dnf -y install $packages
 
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 
@@ -15,7 +29,11 @@ rm -rf ~/.local/share/nvim/lazy/
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
-sudo dnf install "/run/media/$USER/Ventoy/fedora-40-displaylink-1.14.6-1.github_evdi.x86_64.rpm" -y
+wget https://github.com/ShogoXY/fedora/blob/main/fedora-40-displaylink-1.14.6-1.github_evdi.x86_64.rpm
+sudo dnf install fedora-40-displaylink-1.14.6-1.github_evdi.x86_64.rpm -y
+rm fedora-40-displaylink-1.14.6-1.github_evdi.x86_64.rpm
+
+## sudo dnf install "/run/media/$USER/Ventoy/fedora-40-displaylink-1.14.6-1.github_evdi.x86_64.rpm" -y
 
 sudo dnf update
 
@@ -36,7 +54,14 @@ nvim --headless "+Lazy! sync" +qa
 nvim --headless "+Lazy! update" +qa
 echo ""
 mkdir -p $HOME/.fonts
-cp -r /run/media/$USER/Ventoy/fonts $HOME/.fonts
+
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/InconsolataLGC.zip
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Mononoki.zip
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaMono.zip
+
+for f in *.zip; do unzip "$f" -d "$HOME/.fonts/${f%.zip}"; done
+rm *.zip
 
 echo ""
 echo "Installation Complete"
