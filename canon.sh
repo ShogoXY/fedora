9f65127b2c96262164a8d10b32eed0c1efa2115a
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2025-03-10 12:44:22 +0100

    Update canon.sh

--- canon.sh
+++ canon.sh
@@ -26,4 +26,4 @@ alias dslr-webcam v4l2loopback
 options v4l2loopback exclusive_caps=1 max_buffers=2" >> /etc/modprobe.d/dslr-webcam.conf
 
 
-echo "alias canon='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'" >> ~/.bashrc
+echo "alias nikon='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'" >> ~/.bashrc
