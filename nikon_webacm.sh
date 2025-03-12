8c94d30f1286d634e790f033fd6211952cfb5f2b
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2025-03-12 11:52:50 +0100

    automatic USB nikon

--- nikon_webacm.sh
+++ nikon_webacm.sh
@@ -29,7 +29,7 @@ elif [[ $SYS =~ "Fedora" ]]; then
 
 # Automatic connection
 sudo touch /etc/udev/rules.d/99-usb-script.rules
-sudo echo "ACTION==\"add\", SUBSYSTEM==\"usb\", ATTR{idVendor}==\"04b0\", ATTR{idProduct}==\"0448\", RUN+=\"gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \" " >> /etc/udev/rules.d/99-usb-script.rules
+echo "ACTION==\"add\", SUBSYSTEM==\"usb\", ATTR{idVendor}==\"04b0\", ATTR{idProduct}==\"0448\", RUN+=\"gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \" " | sudo tee /etc/udev/rules.d/99-usb-script.rules > /dev/null
 
 
 
