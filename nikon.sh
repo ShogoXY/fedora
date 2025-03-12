3cd50085ae0f3246a31dccb92a2c34d5763877c5
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2025-03-12 11:47:05 +0100

    Add automatic USB connection for Z5

---
+++ nikon.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SYS=` cat /etc/*-release 2> /dev/null | grep PRETTY_NAME `
+de=$(echo $DESKTOP_SESSION)
+
+
+if [[ $SYS =~ "Debian" ]]; then
+
+    sudo apt-get update -y
+    sudo apt-get install gphoto2 v4l2loopback-utils v4l2loopback-dkms ffmpeg
+
+elif [[ $SYS =~ "Fedora" ]]; then
+    sudo dnf update -y
+    sudo dnf install gphoto2 v4l2loopback ffmpeg
+ fi
+     
+
+#sudo apt-get install gphoto2 v4l2loopback-utils v4l2loopback-dkms ffmpeg
+#sudo dnf install gphoto2 v4l2loopback ffmpeg
+
+# Rzeczy potrzebne do DSLR dla CANONA
+
+#sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
+#sudo echo "dslr-webcam" >> /etc/modules
+#sudo touch /etc/modprobe.d/dslr-webcam.conf
+#sudo printf "# Module options for Video4Linux, needed for our DSLR Webcam
+#alias dslr-webcam v4l2loopback
+#options v4l2loopback exclusive_caps=1 max_buffers=2" >> /etc/modprobe.d/dslr-webcam.conf
+
+# Automatic connection
+sudo touch /etc/udev/rules.d/99-usb-script.rules
+sudo echo "ACTION==\"add\", SUBSYSTEM==\"usb\", ATTR{idVendor}==\"04b0\", ATTR{idProduct}==\"0448\", RUN+=\"gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \" " >> /etc/udev/rules.d/99-usb-script.rules
+
+
+
+echo "alias nikon='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'" >> ~/.bashrc
+source ~/.bashrc
